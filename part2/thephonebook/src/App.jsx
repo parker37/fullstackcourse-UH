061732c177d7cf4134d2239e2551575efec7ed7c
parker37 <34007291+parker37@users.noreply.github.com>
2026-06-09 10:20:05 -0400

    part2.13 completed

--- part2/thephonebook/src/App.jsx
+++ part2/thephonebook/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+
+import personsService from "./services/persons"
 
 import Filter from './components/Filter'
 import Persons from './components/Persons'
@@ -12,11 +13,10 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
+    personsService.getAll()
+      .then(initialPersons => {
         console.log('received phonebook')
-        setPersons(response.data)
+        setPersons(initialPersons)
       })
     
   }, [])
@@ -40,14 +40,14 @@ const App = () => {
       number: newNumber
     }
     
-    axios.post('http://localhost:3001/persons', newPerson)
-      .then(response => {
-        console.log(response.data)
-      })
+    personsService.create(newPerson)
+      .then(returnedPerson => {
+        console.log(returnedPerson)
 
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('')
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
   }
 
   const handleNameChange = (event) => {
@@ -86,8 +86,6 @@ const App = () => {
       <Persons
         shownPersons={shownPersons}
       />
-
-      <div>debug: {newName} {newNumber}</div>
     </div>
   )
 }
