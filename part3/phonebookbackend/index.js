92210eab17d7549e696c20ff6c204abd8d4b084c
parker37 <34007291+parker37@users.noreply.github.com>
2026-06-11 02:09:05 -0400

    part3.2 completed

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -32,6 +32,16 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const now = new Date().toString()
+    const people = persons.length === 1 ? `person` : `people`
+
+    response.send(
+        `<p>Phonebook has info for ${persons.length} ${people}</p>\n<p>${now}</p>`
+    )
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`server running on port: ${PORT}`)
